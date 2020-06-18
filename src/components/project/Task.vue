4f8d565a6bcdae2822ee19d45cda9e1f1268faa2
周竞帆 <llczjf@sina.com>
2020-06-18 15:12:47 +0800

    修改了任务状态

--- src/components/project/Task.vue
+++ src/components/project/Task.vue
@@ -26,8 +26,8 @@
             <template v-slot:item.status="{item}">
                 <div v-if="item.status==0">无效</div>
                 <div v-else-if="item.status==1">新建</div>
-                <!-- <div v-else-if="item.status==2">执行中</div> -->
-                <div v-else-if="item.status==2"><a @click="getAllure(item)">执行完成</a></div>
+                <div v-else-if="item.status==2">执行中</div>
+                <div v-else-if="item.status==3"><a @click="getAllure(item)">执行完成</a></div>
             </template>
             
             <template v-slot:item.action="{item}">

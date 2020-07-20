84ecd3352a334db6567882e679bca2d9cca2743a
tlibn <1039085737@qq.com>
2020-07-20 18:56:17 +0800

    在线查看swagger文档

--- src/components/project/ProjectNav.vue
+++ src/components/project/ProjectNav.vue
@@ -8,11 +8,12 @@
                 <v-tab @click="$router.push({name:'Task'})">任务管理</v-tab>
                 <v-tab @click="$router.push({name:'Report'})">报告管理</v-tab>
             </v-tabs>
-            
+
             <v-spacer></v-spacer>
             <v-btn text>{{username}}</v-btn>
+            <v-btn text @click="getSwaggerDoc()">swagger文档</v-btn>
             <v-btn text @click="logout()">退出</v-btn>
-        </v-app-bar>  
+        </v-app-bar>
         <router-view></router-view>
     </div>
 </template>
@@ -34,6 +35,9 @@ export default {
             this.$api.user.logout().then(res=>{
                 this.$router.push('/')
             })
+        },
+        getSwaggerDoc(){
+          window.open(process.env.API+"swagger-ui.html","_blank")
         }
     }
 }

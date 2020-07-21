5584643d51888bbb752d02450c9c5e9e0ae562c5
sigegea <17645026462@163.com>
2020-07-21 11:22:34 +0800

    修改顶部栏大小写和swagger文档显示问题

--- src/components/project/ProjectNav.vue
+++ src/components/project/ProjectNav.vue
@@ -1,17 +1,17 @@
 <template>
     <div>
         <v-app-bar dense dark>
-            <v-toolbar-title style="width:100px">AITEST</v-toolbar-title>
+            <v-toolbar-title style="width:100px" class="text-none">AITEST</v-toolbar-title>
             <v-tabs>
-                <v-tab @click="$router.push('jenkins')">jenkins管理</v-tab>
+                <v-tab @click="$router.push('jenkins')" class="text-none">jenkins管理</v-tab>
                 <v-tab @click="$router.push('case')">用例管理</v-tab>
                 <v-tab @click="$router.push({name:'Task'})">任务管理</v-tab>
                 <v-tab @click="$router.push({name:'Report'})">报告管理</v-tab>
             </v-tabs>
 
             <v-spacer></v-spacer>
-            <v-btn text>{{username}}</v-btn>
-            <v-btn text @click="getSwaggerDoc()">swagger文档</v-btn>
+            <v-btn text class="text-none">{{username}}</v-btn>
+            <v-btn text @click="getSwaggerDoc()" class="text-none" v-if="currentUsername == 't10'">swagger文档</v-btn>
             <v-btn text @click="logout()">退出</v-btn>
         </v-app-bar>
         <router-view></router-view>
@@ -23,12 +23,14 @@ export default {
     data(){
         return {
             username:'',
+            currentUsername:''
         }
     },
     created(){
         this.$api.user.isLogin().then(res=>{
             this.username = res.data.data.userName
         })
+        this.currentUsername = localStorage.getItem('username')
     },
     methods:{
         logout(){
